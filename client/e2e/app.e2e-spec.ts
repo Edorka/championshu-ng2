8c271c02cd99b9f4ab9f0e83ab05e6a68953282b
Eduardo Orive <eduardo.orive@wiseathena.com>
2016-06-27 23:18:35 +0200

    changed generator to angular-cli

---
+++ client/e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { ChampionshuPage } from './app.po';
+
+describe('championshu App', function() {
+  let page: ChampionshuPage;
+
+  beforeEach(() => {
+    page = new ChampionshuPage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
---
+++ client/e2e/app.po.ts
@@ -0,0 +1,9 @@
+export class ChampionshuPage {
+  navigateTo() {
+    return browser.get('/');
+  }
+
+  getParagraphText() {
+    return element(by.css('app-root h1')).getText();
+  }
+}
